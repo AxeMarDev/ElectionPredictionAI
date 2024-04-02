98adb3f4e03d8f7bac7384185608c08364a0837e
Axell Martinez Avila <axell.martinezavila@gmail.com>
2024-04-01 23:41:43 -0500

    added ui elemented 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,11 +15,38 @@ export default function Home() {
 
 
     return (
-      <div className={"bg-gray-800 w-screen h-screen flex justify-center grid content-center"}>
-          <div className="App">
-              <USAMap customize={stateColors()} onClick={mapHandler} />
-          </div>
-      </div>
+        <div>
+            <div className={"bg-gray-800 w-screen h-screen flex flex-col justify-between"}>
+                <div className={"flex flex-col h-full justify-center grid content-center"}>
+                    <USAMap customize={stateColors()} onClick={mapHandler} />
+                </div>
+                <div className={"w-full pl-10  pr-10 flex justify-between "}>
+                    <div className={"flex flex-row"}>
+                        <div className={"  rounded bg-white h-14 w-14"}>                </div>
+                        <p className={"grid content-center pl-5"}>Joseph Robinette Biden</p>
+                    </div>
+                    <div className={"flex flex-row"}>
+                        <p className={"grid content-center pr-5"} >Donald John Trump</p>
+                        <div className={"rounded bg-white h-14 w-14"}>                </div>
+                    </div>
+
+                </div>
+
+                <div className={"flex h-30  pl-10  pr-10 pb-10  pt-6"}>
+                    <div className={"bg-blue-500 w-1/2"}>
+                        <p>bar</p>
+                    </div>
+                    <div className={"bg-red-500 w-1/2"}>
+                        <p>bar</p>
+                    </div>
+                </div>
+            </div>
+            <div className={"h-[23rem]"}>
+                <p>pending</p>
+            </div>
+        </div>
+
+
   );
 }
 
