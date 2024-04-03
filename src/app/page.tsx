fd98427b3b0915e5d55349d89d93340f4dbdea36
Axell Martinez Avila <axell.martinezavila@gmail.com>
2024-04-02 19:06:12 -0500

    Auto stash before merge of "main" and "origin/main"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import USAMap from "react-usa-map";
 import stateColors from "@/app/utility/stateColors";
 
+
 export default function Home() {
 
     const mapHandler = ( event:any ) => {
@@ -22,21 +23,27 @@ export default function Home() {
                 </div>
                 <div className={"w-full pl-10  pr-10 flex justify-between "}>
                     <div className={"flex flex-row"}>
-                        <div className={"  rounded bg-white h-14 w-14"}>                </div>
-                        <p className={"grid content-center pl-5"}>Joseph Robinette Biden</p>
+                        <div className={"  rounded bg-white h-14 w-14"} style={{
+                            backgroundImage: 'url(https://www.whitehouse.gov/wp-content/uploads/2021/04/P20210303AS-1901-cropped.jpg?w=1536)',
+                            backgroundSize: "cover"
+                        }}/>
+                        <p className={"grid content-center pl-5" }>Joseph Robinette Biden</p>
                     </div>
                     <div className={"flex flex-row"}>
                         <p className={"grid content-center pr-5"} >Donald John Trump</p>
-                        <div className={"rounded bg-white h-14 w-14"}>                </div>
+                        <div className={"rounded bg-white h-14 w-14"} style={{
+                            backgroundImage: 'url(https://upload.wikimedia.org/wikipedia/commons/5/56/Donald_Trump_official_portrait.jpg)',
+                            backgroundSize: "cover"
+                        }}/>
                     </div>
 
                 </div>
 
                 <div className={"flex h-30  pl-10  pr-10 pb-10  pt-6"}>
-                    <div className={"bg-blue-500 w-1/2"}>
+                    <div className={"bg-blue-500 w-[45%]"}>
                         <p>bar</p>
                     </div>
-                    <div className={"bg-red-500 w-1/2"}>
+                    <div className={"bg-red-500 w-[55%]"}>
                         <p>bar</p>
                     </div>
                 </div>
